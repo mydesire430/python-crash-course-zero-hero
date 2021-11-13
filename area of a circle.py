dd54156760662b55783edaed91e41ee3cecb8442
mydesire430 <93054243+mydesire430@users.noreply.github.com>
2021-11-13 17:27:19 +0100

    Add files via upload

---
+++ area of a circle.py
@@ -0,0 +1,12 @@
+#Area of a circle
+pie = 3.142
+radius = 7
+area = pie * radius**2
+print(area)
+
+#Volume of a cylinder
+depth = 20
+radius= 2.5
+pie = 3.142
+area = (pie * radius**2 * depth)
+print(area)
