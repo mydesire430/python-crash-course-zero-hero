2071dbaa1e4e6b8dfed53eb562028243081636a9
mydesire430 <93054243+mydesire430@users.noreply.github.com>
2021-11-14 01:30:23 +0100

    Add files via upload

--- area of a circle.py
+++ area of a circle.py
@@ -10,3 +10,25 @@ radius= 2.5
 pie = 3.142
 area = (pie * radius**2 * depth)
 print(area)
+
+#Volume of cuboid
+length = 25
+breadth = 15
+height = 5
+volume = length*breadth*height
+print(volume)
+
+#Area of a triangle
+base = 8
+height =12
+Area = 1/2*base*height
+print(Area)
+
+
+#Volume of concrete
+length = 20
+width = 5
+thickness = 0.225
+volume = length*width*thickness
+print(volume)
+
