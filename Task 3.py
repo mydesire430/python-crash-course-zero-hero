f8eb23a174553d3d3823624f81321c69509962cf
mydesire430 <93054243+mydesire430@users.noreply.github.com>
2021-11-20 19:32:14 +0100

    Add files via upload

---
+++ Task 3.py
@@ -0,0 +1,45 @@
+
+X = 'Hello there, how old are you?'
+print(X[13:20])
+
+story = "python is awesome"
+print(story[2:4] + story[-1])
+
+mystring = "python rocks"
+print(len(mystring))
+
+flower = "Rose"
+#print(flower[0])
+
+word = "python is so cool"
+print(word[-4:]*3)
+
+word = "pepsi"
+print(word.upper())
+
+word = "macdonald"
+print(word[0].upper() + word[1:3].lower() + word[3].upper() + word[4:].lower())
+
+word = "MUSHRAH"
+print(word.lower())
+
+my_string = "Hello World"
+print(my_string.split())
+
+my_string = "python is cool"
+print('-'.join(my_string))
+
+my_string = "Hello world"
+print(my_string[0:5])
+
+mystring="Sulaiman"
+print(mystring.index("S"))
+
+#using one of the string built-in methods,print out the numbers of 'o' in the string; "Hello, python is soo cool"?
+mystring = "Hello, python is sooo coool"
+print(mystring.count('o'))
+
+#If i want to write this string:"hello world! welcome to python" as a title case, what inbuilt method of python will i call?
+title_case = "hellow world! welcome to python"
+print(title_case.title())
+
